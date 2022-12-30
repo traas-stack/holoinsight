3b55a2c453b235e65d99c8fc5c372088dff0aa56
xzchaoo <xzchaoo@gmail.com>
2022-12-30 16:53:17 +0800

    feat: migrate all-in-one module

---
+++ server/all-in-one/all-in-one-bootstrap/src/test/java/io/holoinsight/server/allinone/TestHoloinsightAllInOneBootstrap.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2022 Holoinsight Project Authors. Licensed under Apache-2.0.
+ */
+package io.holoinsight.server.allinone;
+
+import io.holoinsight.server.allinone.bootstrap.HoloinsightAllInOneBootstrap;
+import io.holoinsight.server.common.ContextHolder;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * <p>created at 2022/11/25
+ *
+ * @author xzchaoo
+ */
+public class TestHoloinsightAllInOneBootstrap {
+    public static void main(String[] args) {
+        ContextHolder.ctx = SpringApplication.run(HoloinsightAllInOneBootstrap.class);
+    }
+}
