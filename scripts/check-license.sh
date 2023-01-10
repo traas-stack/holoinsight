7e512c4c38abe1504bbfca3e349e589956ff2bd1
xzchaoo <xzchaoo@gmail.com>
2023-01-10 20:29:35 +0800

    ci: add format and license check (#35)

---
+++ scripts/check-license.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# usage: add license for all java sources
+
+script_dir=`dirname $0`
+project_root=`realpath $script_dir/..`
+
+cd $project_root
+
+mvn -f server/server-parent/pom.xml license:check
