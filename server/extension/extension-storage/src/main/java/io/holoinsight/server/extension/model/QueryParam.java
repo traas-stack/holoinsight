9cb319f70d94a7c57fb50a5421c6dafd8c832515
jiwliu <cc947535559@gmail.com>
2023-01-17 17:13:35 +0800

    feat(storage): add interface for time series storage

---
+++ server/extension/extension-storage/src/main/java/io/holoinsight/server/extension/model/QueryParam.java
@@ -0,0 +1,35 @@
+package io.holoinsight.server.extension.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+/**
+ * @author jinyan.ljw
+ * @date 2023/1/11
+ */
+@Data
+public class QueryParam {
+  private String tenant;
+  long start;
+  long end;
+  String metric;
+  List<QueryFilter> filters;
+  String aggregator;
+  String downsample;
+  SlidingWindow slidingWindow;
+  List<String> groupBy;
+
+  @Data
+  public static class SlidingWindow {
+    int windowMs;
+    String aggregator;
+  }
+
+  @Data
+  public static class QueryFilter {
+    String type;
+    String name;
+    String value;
+  }
+}
