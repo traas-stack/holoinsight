28e4fb87a57ae825e2e72d202c2e1756b7b8bed5
xzchaoo <xzchaoo@gmail.com>
2023-02-02 17:16:37 +0800

    feat(home): add front-end files (#89)

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -6,6 +6,6 @@ project_root=`realpath $script_dir/../..`
 
 cd $project_root
 
-#sh ./scripts/all-in-one/build.sh
+sh ./scripts/all-in-one/build.sh
 
 docker build --network host -t holoinsight/server:temp -f ./scripts/docker/Dockerfile .
