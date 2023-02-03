f41c0bc8f15900dedc6a6dfb32a3290614a85a53
jiwliu <cc947535559@gmail.com>
2023-02-03 17:46:28 +0800

    Feat/support ceresdb4 (#100)
    * feat(metrics): support ceresdb4.0 metrics storage

--- server/extension/extension-storage-ceresdbx/src/main/java/io/holoinsight/server/extension/ceresdbx/HoloinsightCeresdbxConfiguration.java
+++ server/extension/extension-storage-ceresdbx/src/main/java/io/holoinsight/server/extension/ceresdbx/HoloinsightCeresdbxConfiguration.java
@@ -3,15 +3,28 @@
  */
 package io.holoinsight.server.extension.ceresdbx;
 
+import io.holoinsight.server.common.dao.mapper.TenantOpsMapper;
+import io.holoinsight.server.extension.MetricStorage;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @author jinyan.ljw
+ * @author jiwliu
  * @date 2023/1/10
  */
 @Configuration
 @ConditionalOnProperty(value = "holoinsight.metric.storage.type", havingValue = "ceresdbx")
 public class HoloinsightCeresdbxConfiguration {
+
+  @Bean
+  public CeresdbxClientManager ceresdbxClientManager(TenantOpsMapper tenantOpsMapper) {
+    return new CeresdbxClientManager(tenantOpsMapper);
+  }
+
+  @Bean
+  public MetricStorage ceresdbxMetricStorage(CeresdbxClientManager ceresdbxClientManager) {
+    return new CeresdbxMetricStorage(ceresdbxClientManager);
+  }
 
 }
