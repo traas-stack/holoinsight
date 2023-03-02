3b9c2a35238ca8278e8e85e9deb83d7fb28dfaa4
saimu.msm <saimu.msm@antfin.com>
2023-03-02 17:25:15 +0800

    change httpclient

--- server/home/home-alert/src/main/java/io/holoinsight/server/home/alert/service/calculate/AlgorithmHttp.java
+++ server/home/home-alert/src/main/java/io/holoinsight/server/home/alert/service/calculate/AlgorithmHttp.java
@@ -3,13 +3,20 @@
  */
 package io.holoinsight.server.home.alert.service.calculate;
 
-import io.holoinsight.server.home.alert.common.http.HttpProxy;
-import io.holoinsight.server.home.alert.common.http.XHttpRequest;
-import io.holoinsight.server.home.alert.common.http.XHttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author wangsiyuan
@@ -19,23 +26,69 @@ public class AlgorithmHttp {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AlgorithmHttp.class);
 
+  private static final RequestConfig requestConfig;
+
+  static {
+    requestConfig = RequestConfig.custom().setConnectTimeout(1000 * 60)
+        .setConnectionRequestTimeout(1000 * 6).setSocketTimeout(1000 * 60 * 3).build();
+  }
+
   public static String invokeAlgorithm(String algorithmUrl, String requestBody) {
-    XHttpResponse respose = null;
+    Map<String, String> headers = new HashMap<>();
+    headers.put("Content-Type", "application/json");
+    return post(algorithmUrl, requestBody, headers);
+  }
+
+  public static String post(String algorithmUrl, String requestBody, Map<String, String> headers) {
+    CloseableHttpClient httpClient = HttpClients.createDefault();
+    CloseableHttpResponse response = null;
+    String resultString = null;
     try {
-      XHttpRequest req = XHttpRequest.post(algorithmUrl, Collections.emptyMap(), "utf-8", 5000,
-          requestBody.getBytes(), "application/json; charset=utf-8");
-      respose = HttpProxy.request(req);
-      if (respose.code != 200) {
-        LOGGER.error("[InvokeAlgorithmError],AlgorithmUrl={},requestBody={},response={},content={}",
-            algorithmUrl, requestBody, respose.code, respose.getStringResponse());
-        return null;
-      } else {
-        LOGGER.info("[InvokeAlgorithmInfo],AlgorithmUrl={},requestBody={},response={},content={}",
-            algorithmUrl, requestBody, respose.code, respose.getStringResponse());
+      long start = System.currentTimeMillis();
+      HttpPost httpPost = new HttpPost(algorithmUrl);
+      if (headers != null) {
+        for (String key : headers.keySet()) {
+          httpPost.addHeader(key, headers.get(key));
+        }
       }
+      httpPost.setConfig(requestConfig);
+      if (requestBody != null) {
+        StringEntity entity = new StringEntity(requestBody,
+            ContentType.create(ContentType.APPLICATION_JSON.getMimeType(), "utf-8"));
+        httpPost.setEntity(entity);
+      }
+      response = httpClient.execute(httpPost);
+      long cost = System.currentTimeMillis() - start;
+      if (response != null) {
+        resultString = EntityUtils.toString(response.getEntity(), "UTF-8");
+        if (response.getStatusLine().getStatusCode() != 200) {
+          LOGGER.error(
+              "[InvokeAlgorithmError],AlgorithmUrl={},requestBody={},response={},content={},cost={}",
+              algorithmUrl, requestBody, response.getStatusLine().getStatusCode(), resultString,
+              cost);
+          return null;
+        } else {
+          LOGGER.info(
+              "[InvokeAlgorithmInfo],AlgorithmUrl={},requestBody={},response={},content={},cost={}",
+              algorithmUrl, requestBody, response.getStatusLine().getStatusCode(), resultString,
+              cost);
+        }
+      } else {
+        LOGGER.error("[InvokeAlgorithmError],AlgorithmUrl={}, response is null,cost={}",
+            algorithmUrl, cost);
+      }
+
     } catch (Exception e) {
-      LOGGER.error("[InvokeAlgorithmException] Sync Exception", e);
+      LOGGER.error("[InvokeAlgorithmException] Sync Exception for {}", algorithmUrl, e);
+    } finally {
+      try {
+        if (response != null) {
+          response.close();
+        }
+      } catch (IOException e) {
+        LOGGER.error("[InvokeAlgorithmException] Http close fail for {}", algorithmUrl, e);
+      }
     }
-    return respose == null ? null : respose.getStringResponse();
+    return resultString;
   }
 }
