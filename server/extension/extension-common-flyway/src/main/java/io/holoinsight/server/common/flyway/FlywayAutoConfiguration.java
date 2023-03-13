69a6071700dcfad6501932797f0354d5e659905d
jiwliu <cc947535559@gmail.com>
2023-03-13 16:28:57 +0800

    feat: add extension-common-flyway module

--- server/extension/extension-common-flyway/src/main/java/io/holoinsight/server/common/flyway/FlywayAutoConfiguration.java
+++ server/extension/extension-common-flyway/src/main/java/io/holoinsight/server/common/flyway/FlywayAutoConfiguration.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2022 Holoinsight Project Authors. Licensed under Apache-2.0.
  */
-package io.holoinsight.server.common.service.flyway;
+package io.holoinsight.server.common.flyway;
 
 import javax.sql.DataSource;
 
@@ -12,18 +12,17 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
+ * {@link EnableAutoConfiguration Auto-configuration} for Flyway database migrations.
+ * 
  * @author jiwliu
  * @date 2023/3/1
  */
-
-@Configuration(proxyBeanMethods = false)
 @AutoConfigureAfter({DataSourceAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
 @EnableConfigurationProperties(FlywayProperties.class)
 @ConditionalOnProperty(prefix = "holoinsight.flyway", name = "enabled", havingValue = "true")
-public class FlywayConfiguration {
+public class FlywayAutoConfiguration {
 
   @Bean
   public FlywayService flywayService(FlywaySchemaHistoryMapper flywaySchemaHistoryMapper,
