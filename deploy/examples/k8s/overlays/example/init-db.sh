e5af93f641f27420b4a6f437be11c472822f6ed8
xzchaoo <xzchaoo@gmail.com>
2023-03-16 13:04:23 +0800

    fix: fix k8s demo db init error

--- deploy/examples/k8s/overlays/example/init-db.sh
+++ deploy/examples/k8s/overlays/example/init-db.sh
@@ -10,7 +10,7 @@ echo [database] wait for [mysql] to be available
 kubectl -n $ns rollout status statefulset/mysql
 
 echo [database] initialize table structures
-kubectl -n $ns exec -i mysql-0 -- mysql -uholoinsight -pholoinsight -Dholoinsight < $script_dir/../../../docker-compose/ddl.sql 2>/dev/null
+kubectl -n $ns exec -i mysql-0 -- mysql -uholoinsight -pholoinsight -Dholoinsight < $script_dir/../../../../../server/extension/extension-common-flyway/src/main/resources/db/migration/V1__230301_INIT_DDL.sql 2>/dev/null
 
 echo [database] populate example data
 kubectl -n $ns exec -i mysql-0 -- mysql -uholoinsight -pholoinsight -Dholoinsight < $script_dir/../../../docker-compose/data.sql 2>/dev/null
