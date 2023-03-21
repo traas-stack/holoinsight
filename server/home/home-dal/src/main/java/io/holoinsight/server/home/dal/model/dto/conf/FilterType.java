a6387d18be3149940e1d3966a9767eb588547c92
霄鸿 <jinsong.yjs@antfin.com>
2023-03-21 15:54:18 +0800

    feat: Enhance the collection and filtering capability of logmonitor

---
+++ server/home/home-dal/src/main/java/io/holoinsight/server/home/dal/model/dto/conf/FilterType.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2022 Holoinsight Project Authors. Licensed under Apache-2.0.
+ */
+
+package io.holoinsight.server.home.dal.model.dto.conf;
+
+/**
+ * <p>
+ *     collect log filter type
+ * </p>
+ * <p>
+ * Date: 2023-03-21 Time: 10:50
+ * </p>
+ *
+ * @author jsy1001de
+ */
+public enum FilterType {
+  IN, NOT_IN
+
+}
