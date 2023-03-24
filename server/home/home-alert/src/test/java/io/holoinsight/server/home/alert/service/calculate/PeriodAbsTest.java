70493c7feef45d6ff2e4d03733638d8951103054
masaimu <masaimu@126.com>
2023-03-24 12:49:02 +0800

    add license

--- server/home/home-alert/src/test/java/io/holoinsight/server/home/alert/service/calculate/PeriodAbsTest.java
+++ server/home/home-alert/src/test/java/io/holoinsight/server/home/alert/service/calculate/PeriodAbsTest.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2022 Holoinsight Project Authors. Licensed under Apache-2.0.
+ */
 package io.holoinsight.server.home.alert.service.calculate;
 
 import io.holoinsight.server.home.alert.model.function.FunctionConfigParam;
