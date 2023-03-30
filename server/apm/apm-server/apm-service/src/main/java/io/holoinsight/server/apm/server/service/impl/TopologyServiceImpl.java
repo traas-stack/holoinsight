3495c27b39eaf9f7810da1a08c0830810d034cd6
Alan <xwanpeng@gmail.com>
2023-03-30 19:29:32 +0800

    fix: fix the topology exception of the endpoint (#298)

--- server/apm/apm-server/apm-service/src/main/java/io/holoinsight/server/apm/server/service/impl/TopologyServiceImpl.java
+++ server/apm/apm-server/apm-service/src/main/java/io/holoinsight/server/apm/server/service/impl/TopologyServiceImpl.java
@@ -62,8 +62,8 @@ public class TopologyServiceImpl implements TopologyService {
   @Override
   public Topology getServiceInstanceTopology(String tenant, String service, String serviceInstance,
       long startTime, long endTime, int depth, Map<String, String> termParams) throws Exception {
-    List<Call.DeepCall> calls = getDeepCalls(topologyStorage, tenant, service, serviceInstance,
-        null, startTime, endTime, depth, termParams);
+    List<Call.DeepCall> calls = getDeepCalls(topologyStorage::getServiceInstanceCalls, tenant,
+        service, serviceInstance, null, startTime, endTime, depth, termParams);
 
     Topology topology = buildServiceInstanceTopo(calls);
     setNodeMetric(tenant, topology.getNodes(), startTime, endTime, termParams);
@@ -75,56 +75,66 @@ public class TopologyServiceImpl implements TopologyService {
   @Override
   public Topology getEndpointTopology(String tenant, String service, String endpoint,
       long startTime, long endTime, int depth, Map<String, String> termParams) throws Exception {
-    List<Call.DeepCall> calls = getDeepCalls(topologyStorage, tenant, service, null, endpoint,
-        startTime, endTime, depth, termParams);
+    List<Call.DeepCall> calls = getDeepCalls(topologyStorage::getEndpointCalls, tenant, service,
+        null, endpoint, startTime, endTime, depth, termParams);
 
     Topology topology = buildEndpointTopo(calls);
     setNodeMetric(tenant, topology.getNodes(), startTime, endTime, termParams);
     return topology;
   }
 
-  protected List<Call.DeepCall> getDeepCalls(TopologyStorage storage, String tenant, String service,
+  public interface CallsLoader {
+    List<Call.DeepCall> loadCalls(String tenant, String service, String instanceOrEndpoint,
+        long startTime, long endTime, String sourceOrDest, Map<String, String> termParams)
+        throws IOException;
+  }
+
+
+  protected List<Call.DeepCall> getDeepCalls(CallsLoader callsLoader, String tenant, String service,
       String serviceInstance, String endpoint, long startTime, long endTime, int depth,
       Map<String, String> termParams) throws IOException {
     List<Call.DeepCall> calls = new ArrayList<>();
-    List<Call.DeepCall> sourceEndpointList = new ArrayList<>();
-    Call.DeepCall sourceEndpoint = new Call.DeepCall();
-    sourceEndpoint.setSourceServiceName(service);
+    List<Call.DeepCall> sourceCalls = new ArrayList<>();
+    Call.DeepCall sourceCall = new Call.DeepCall();
+    sourceCall.setSourceServiceName(service);
     if (serviceInstance != null) {
-      sourceEndpoint.setSourceName(serviceInstance);
+      sourceCall.setSourceName(serviceInstance);
     }
     if (endpoint != null) {
-      sourceEndpoint.setSourceName(endpoint);
+      sourceCall.setSourceName(endpoint);
     }
-    sourceEndpointList.add(sourceEndpoint);
+    sourceCalls.add(sourceCall);
 
-    List<Call.DeepCall> destEndpointList = new ArrayList<>();
-    Call.DeepCall destEndpoint = new Call.DeepCall();
-    destEndpoint.setDestServiceName(service);
-    destEndpoint.setDestName(serviceInstance);
-    destEndpointList.add(destEndpoint);
+    List<Call.DeepCall> destCalls = new ArrayList<>();
+    Call.DeepCall destCall = new Call.DeepCall();
+    destCall.setDestServiceName(service);
+    if (serviceInstance != null) {
+      destCall.setDestName(serviceInstance);
+    }
+    if (endpoint != null) {
+      destCall.setDestName(endpoint);
+    }
+    destCalls.add(destCall);
 
     for (int i = depth; i > 0; i--) {
-      List<Call.DeepCall> newSourceEndpointList = new ArrayList<>();
-      for (Call.DeepCall item : sourceEndpointList) {
-        newSourceEndpointList
-            .addAll(storage.getServiceInstanceCalls(tenant, item.getSourceServiceName(),
-                item.getSourceName(), startTime, endTime, Const.DEST, termParams));
+      List<Call.DeepCall> newSourceCalls = new ArrayList<>();
+      for (Call.DeepCall item : sourceCalls) {
+        newSourceCalls.addAll(callsLoader.loadCalls(tenant, item.getSourceServiceName(),
+            item.getSourceName(), startTime, endTime, Const.DEST, termParams));
       }
-      sourceEndpointList.clear();
-      sourceEndpointList.addAll(newSourceEndpointList);
+      sourceCalls.clear();
+      sourceCalls.addAll(newSourceCalls);
 
-      List<Call.DeepCall> newDestEndpointList = new ArrayList<>();
-      for (Call.DeepCall item : destEndpointList) {
-        newDestEndpointList
-            .addAll(storage.getServiceInstanceCalls(tenant, item.getDestServiceName(),
-                item.getDestName(), startTime, endTime, Const.SOURCE, termParams));
+      List<Call.DeepCall> newDestCalls = new ArrayList<>();
+      for (Call.DeepCall item : destCalls) {
+        newDestCalls.addAll(callsLoader.loadCalls(tenant, item.getDestServiceName(),
+            item.getDestName(), startTime, endTime, Const.SOURCE, termParams));
       }
-      destEndpointList.clear();
-      destEndpointList.addAll(newDestEndpointList);
+      destCalls.clear();
+      destCalls.addAll(newDestCalls);
 
-      calls.addAll(newSourceEndpointList);
-      calls.addAll(newDestEndpointList);
+      calls.addAll(newSourceCalls);
+      calls.addAll(newDestCalls);
     }
     return calls;
   }
