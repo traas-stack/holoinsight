3cc9d7b338c893748f631bc42f8221c10c96f3be
xzchaoo <xzchaoo@gmail.com>
2023-04-11 19:47:53 +0800

    test: fix IT (#343)

--- scripts/test/e2e/one.sh
+++ scripts/test/e2e/one.sh
@@ -15,11 +15,31 @@ if [ -z "$it_class" ]; then
   exit 1
 fi
 
-if [ "$build"x = "1"x ]; then
-  ./scripts/docker/build.sh
+# if $HOLOINSIGHT_DEV is not empty, then it is used as a prefix of docker images and containers to avoid conflicts with others.
+if [ -n "$HOLOINSIGHT_DEV" ]; then
+  tag="dev-$HOLOINSIGHT_DEV"
+  export COMPOSE_server_image=holoinsight/server:$tag
+  if [ "$build"x = "1"x ]; then
+      holoinsight_server_tag=$tag ./scripts/docker/build.sh
+  fi
+else
+  if [ "$build"x = "1"x ]; then
+    ./scripts/docker/build.sh
+  fi
 fi
 
-IT_CLASS=$it_class mvn clean integration-test -f server/server-parent -am -pl ../../test/server-e2e-test -Dgroups=e2e-one -DskipITs=false
+if [ "$build"x = "1"x ] && [ -z "$MVN_NO_CLEAN" ]; then
+  # There is no need to clean when `build=1`
+  MVN_NO_CLEAN='1'
+fi
+
+if [ "$MVN_NO_CLEAN"x = "1"x ]; then
+  goals="integration-test"
+else
+  goals="clean integration-test"
+fi
+
+MAVEN_OPTS="-Xmx512m" IT_CLASS=$it_class mvn $goals -f server/server-parent -am -pl ../../test/server-e2e-test -Dgroups=e2e-one -DskipITs=false
 
 summary=./test/server-e2e-test/target/failsafe-reports/failsafe-summary.xml
 cat $summary
