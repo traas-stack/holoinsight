e8de04eda7e5eec90b74da3ebfde4d2d33fd820d
sanhuan.sw <sanhuan.sw@alibaba-inc.com>
2023-04-12 17:45:12 +0800

    chore: add license

--- server/apm/apm-server/apm-engine/src/main/java/io/holoinsight/server/apm/engine/storage/ICommonBuilder.java
+++ server/apm/apm-server/apm-engine/src/main/java/io/holoinsight/server/apm/engine/storage/ICommonBuilder.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2022 Holoinsight Project Authors. Licensed under Apache-2.0.
+ */
 package io.holoinsight.server.apm.engine.storage;
 
 import io.holoinsight.server.apm.common.model.query.ResponseMetric;
