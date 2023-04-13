6c3c909112a7b6f7df4ca5dc853e0396c82973d2
sw1136562366 <sw1136562366@gmail.com>
2023-04-13 17:12:20 +0800

    fix: metric is not exist (#363)

--- test/server-e2e-test/src/main/java/io/holoinsight/server/test/it/MetricMonitoringIT.java
+++ test/server-e2e-test/src/main/java/io/holoinsight/server/test/it/MetricMonitoringIT.java
@@ -16,9 +16,42 @@ import io.restassured.response.ValidatableResponse;
 public class MetricMonitoringIT extends BaseIT {
 
   /**
-   * Test query tagValues
+   * Test query not exist metric
    */
   @Order(1)
+  @Test
+  public void test_query_not_exists_metrics_monitoring() {
+    await("test query not exist metric") //
+        .untilAsserted(() -> {
+          long end = System.currentTimeMillis() / 60000 * 60000;
+          long start = end - 60000;
+          String name = "test_query_not_exists_metrics_monitoring";
+          String metric = "not_exist_metric_xxx";
+          JSONObject body = json() //
+              .put("query", name) //
+              .put("datasources", jarray() //
+                  .put(json() //
+                      .put("name", name) //
+                      .put("metric", metric) //
+                      .put("aggregator", "none") //
+                      .put("start", start) //
+                      .put("end", end + 60_000) //
+                      .put("downsample", "5s").put("aggregator", "avg").put("filters", jarray()) //
+                      .put("groupBy", jarray().put("app").put("ip")) //
+          ));
+          given() //
+              .body(body) //
+              .when() //
+              .post("/webapi/v1/query") //
+              .then() //
+              .body("success", IS_TRUE);
+        });
+  }
+
+  /**
+   * Test query tagValues
+   */
+  @Order(2)
   @Test
   public void test_query_tag_values_monitoring_item() {
     await("test query tag values") //
