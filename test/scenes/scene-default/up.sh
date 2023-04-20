711fe313c978bef079bc85e471d4caef99116a7d
xzchaoo <xzchaoo@gmail.com>
2023-04-20 14:36:10 +0800

    test: automatically enable debug mode when developing (#385)

--- test/scenes/scene-default/up.sh
+++ test/scenes/scene-default/up.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -n "$HOLOINSIGHT_DEV" ]; then
+  # automatically enable debug mode when developing
+  debug=1
+fi
+
 source `dirname $0`/../common/`basename $0`
--- test/server-e2e-test/src/main/java/io/holoinsight/server/test/it/env/DockerComposeEnv.java
+++ test/server-e2e-test/src/main/java/io/holoinsight/server/test/it/env/DockerComposeEnv.java
@@ -4,6 +4,7 @@
 package io.holoinsight.server.test.it.env;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -67,9 +68,31 @@ public class DockerComposeEnv implements Env<DockerComposeEnv> {
 
     long begin = System.currentTimeMillis();
     log.info("docker-compose up file={}", composeFile.getAbsolutePath());
-    dcc.start();
-    long cost = System.currentTimeMillis() - begin;
-    log.info("docker-compose bootstrap success, cost=[{}]ms", cost);
+    try {
+      dcc.start();
+      long cost = System.currentTimeMillis() - begin;
+      log.info("docker-compose bootstrap success, cost=[{}]ms", cost);
+    } finally {
+      try {
+        new ProcessExecutor("free", "-m") //
+            .redirectOutput(System.out) //
+            .redirectError(System.err) //
+            .timeout(10, TimeUnit.MINUTES) //
+            .execute();
+      } catch (IOException e) {
+        log.error("free -m error", e);
+      }
+
+      try {
+        new ProcessExecutor("docker", "ps", "-a") //
+            .redirectOutput(System.out) //
+            .redirectError(System.err) //
+            .timeout(10, TimeUnit.MINUTES) //
+            .execute();
+      } catch (IOException e) {
+        log.error("docker ps -a", e);
+      }
+    }
 
 
     File after = new File(composeFile.getParentFile(), "after.sh");
@@ -92,14 +115,6 @@ public class DockerComposeEnv implements Env<DockerComposeEnv> {
       }
     }
 
-    if (new File("/usr/bin/free").canExecute()) {
-      ProcessResult result = new ProcessExecutor("/usr/bin/free", "-m") //
-          .readOutput(true) //
-          .timeout(10, TimeUnit.MINUTES) //
-          .execute(); //
-
-      log.info("free -m:\n{}", result.getOutput().getUTF8());
-    }
   }
 
   @Override
