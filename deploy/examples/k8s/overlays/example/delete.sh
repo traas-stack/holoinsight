099905d6a02df9d194a1e6721a4ea755b92aac7f
xzchaoo <xzchaoo@gmail.com>
2023-04-25 12:43:55 +0800

    fix: fix k8s demo

--- deploy/examples/k8s/overlays/example/delete.sh
+++ deploy/examples/k8s/overlays/example/delete.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-set -e
 
 ns=holoinsight-example
 
 kubectl -n $ns delete statefulset mongo mysql holoinsight-server-example clusteragent ceresdb
 kubectl -n $ns delete daemonset cadvisor daemonagent
-kubectl -n $ns delete service registry gateway mongo mysql ceresdb
-kubectl -n $ns delete configmap ceresdb-config holoinsight-agent-cm mongo-cm server-cm
+kubectl -n $ns delete service registry gateway mongo mysql ceresdb service-collector prometheus
+kubectl -n $ns delete deployment collector prometheus
+kubectl -n $ns delete configmap holoinsight-agent-cm mongo-cm server-cm collector-config prometheus-cm
 kubectl delete clusterrolebinding holoinsight-agent-example-clusterrolebinding
 kubectl delete clusterrole holoinsight-agent-example-clusterrole
 
--- deploy/examples/k8s/overlays/example/init-db.sh
+++ deploy/examples/k8s/overlays/example/init-db.sh
@@ -3,15 +3,16 @@ set -e
 
 script_dir=`dirname $0`
 
-ns=holoinsight-example
+export ns=holoinsight-example
 
 echo
 echo [database] wait for [mysql] to be available
 kubectl -n $ns rollout status statefulset/mysql
 
 echo [database] initialize table structures
-kubectl -n $ns exec -i mysql-0 -- mysql -uholoinsight -pholoinsight -Dholoinsight < $script_dir/../../../../../server/extension/extension-common-flyway/src/main/resources/db/migration/V1__230301_INIT_DDL.sql 2>/dev/null
+
+find $script_dir/../../../../../server/extension/extension-common-flyway/src/main/resources/db/migration -name "*.sql" | sort | xargs -I {} $script_dir/mysql-exec.sh {}
 
 echo [database] populate example data
-kubectl -n $ns exec -i mysql-0 -- mysql -uholoinsight -pholoinsight -Dholoinsight < $script_dir/../../../docker-compose/data.sql 2>/dev/null
+$script_dir/mysql-exec.sh $script_dir/../../../../../test/scenes/scene-default/data.sql
 echo
