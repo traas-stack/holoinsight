6d174f91a516d7b5bfa073c3605bba4b13b7eeec
霄鸿 <jinsong.yjs@antfin.com>
2023-05-18 09:54:43 +0800

    feat: Optimize the crawlerTask

--- server/home/home-dal/src/main/java/io/holoinsight/server/home/dal/model/dto/IntegrationMetricDTO.java
+++ server/home/home-dal/src/main/java/io/holoinsight/server/home/dal/model/dto/IntegrationMetricDTO.java
@@ -3,7 +3,9 @@
  */
 package io.holoinsight.server.home.dal.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
@@ -11,6 +13,8 @@ import lombok.Data;
  * @version : IntegrationProductDTO.java, v 0.1 2022年06月08日 16:41 xiangwanpeng Exp $
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IntegrationMetricDTO {
   private String name;
   private String unit;
