53ff8587d53c5a2e854beac686cc8db29e9ce7ea
xiangwanpeng <xwanpeng@gmail.com>
2023-05-29 21:24:52 +0800

    fix: fix import

--- server/apm/apm-server/apm-engine/src/main/java/io/holoinsight/server/apm/engine/tatris/ttl/TatrisDataCleaner.java
+++ server/apm/apm-server/apm-engine/src/main/java/io/holoinsight/server/apm/engine/tatris/ttl/TatrisDataCleaner.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import retrofit2.Response;
 
 /**
  * @author xiangwanpeng
