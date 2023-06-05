2cd7a54332b724031e3788705cb7c3bd40d3e8ef
霄鸿 <jinsong.yjs@antfin.com>
2023-06-05 11:05:21 +0800

    fix: sovle problem of agent logtail

--- server/home/home-service/src/main/java/io/holoinsight/server/home/biz/service/agent/AgentLogTailService.java
+++ server/home/home-service/src/main/java/io/holoinsight/server/home/biz/service/agent/AgentLogTailService.java
@@ -51,8 +51,8 @@ public class AgentLogTailService {
 
     FileTailResponse response = new FileTailResponse();
 
-    List<FileNode> fileNodes =
-        registryService.listFiles(tenant, dim, agentParamRequest.getLogpath());
+    List<FileNode> fileNodes = registryService.listFiles(
+        tenantInitService.getTenantServerTable(tenant), dim, agentParamRequest.getLogpath());
 
     response.addToDatas("dirTrees", convertFiledNodes(fileNodes));
     response.addToDatas("agentId", dim.get("agentId"));
@@ -69,8 +69,8 @@ public class AgentLogTailService {
     Map<String, Object> dim = getDimByRequest(agentParamRequest, tenant, workspace);
     FileTailResponse response = new FileTailResponse();
 
-    PreviewFileResponse grpcResp =
-        registryService.previewFile(tenant, dim, agentParamRequest.getLogpath());
+    PreviewFileResponse grpcResp = registryService.previewFile(
+        tenantInitService.getTenantServerTable(tenant), dim, agentParamRequest.getLogpath());
 
     response.addToDatas("lines", convertContentLines(grpcResp.getContentList()));
     response.addToDatas("charset", grpcResp.getCharset());
@@ -87,8 +87,9 @@ public class AgentLogTailService {
 
     Map<String, Object> dim = getDimByRequest(agentParamRequest, tenant, workspace);
     FileTailResponse response = new FileTailResponse();
-    String result = RetryUtils.invoke(() -> registryService.inspect(tenant, dim), null, 3, 1000,
-        new ArrayList<>());
+    String result = RetryUtils.invoke(
+        () -> registryService.inspect(tenantInitService.getTenantServerTable(tenant), dim), null, 3,
+        1000, new ArrayList<>());
 
     Map<String, Object> map = J.toMap(result);
 
