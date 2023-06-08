afd0cc26a33bc4751721d67e0ce6ddd34f3bb843
xzchaoo <xzchaoo@gmail.com>
2023-06-08 17:47:39 +0800

    chore: remove redundant code

--- deploy/examples/docker-compose/exec-sql-script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo exec $1
-
-mysql -h mysql -uholoinsight -pholoinsight -Dholoinsight < $1
--- deploy/examples/docker-compose/init-db.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd `dirname $0`
-
-find /sql -name "*.sql" | sort | xargs -I {} /exec-sql-script.sh {}
