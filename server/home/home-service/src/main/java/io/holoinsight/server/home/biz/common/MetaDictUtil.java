af3a3fcf0aa9a3ab2e8ea6f58043e0f96c56b21f
Haibo Zang <zhb_4427@126.com>
2023-06-09 16:12:41 +0800

    feat: add meter submit trigger (#484)

--- server/home/home-service/src/main/java/io/holoinsight/server/home/biz/common/MetaDictUtil.java
+++ server/home/home-service/src/main/java/io/holoinsight/server/home/biz/common/MetaDictUtil.java
@@ -147,4 +147,22 @@ public class MetaDictUtil {
     }
     return value;
   }
+
+  public static Boolean isMeteringCloudrunLogSubmitOpen() {
+    Boolean value = MetaDictUtil.getValue(MetaDictType.GLOBAL_CONFIG,
+        MetaDictKey.METERING_CLOUDRUN_LOG_SUBMIT_OPEN, new TypeToken<Boolean>() {});
+    if (null == value) {
+      return false;
+    }
+    return value;
+  }
+
+  public static Boolean isMeteringHoloinsightSubmitOpen() {
+    Boolean value = MetaDictUtil.getValue(MetaDictType.GLOBAL_CONFIG,
+        MetaDictKey.METERING_HOLOINSIGHT_SUBMIT_OPEN, new TypeToken<Boolean>() {});
+    if (null == value) {
+      return false;
+    }
+    return value;
+  }
 }
