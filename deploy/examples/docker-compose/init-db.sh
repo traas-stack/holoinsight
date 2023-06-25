5e719fab47d93beb82519b92747d5015390ddd05
saimu.msm <saimu.msm@antfin.com>
2023-06-25 19:21:56 +0800

    fix db init sh

--- deploy/examples/docker-compose/init-db.sh
+++ deploy/examples/docker-compose/init-db.sh
@@ -3,4 +3,4 @@ set -e
 
 cd `dirname $0`
 
-find /sql -name "*.sql" | awk -F '__' '/^V/ {print $1,$2}' | sort -k 1.2,1.5n | awk '{print $1 "__" $2}' | xargs -I {} /exec-sql-script.sh {}
+find /sql -name "*.sql"  | awk -F '__|/V' '{print $1, $2, $3}'|sort -k 2.1,2.4n | awk '{print $1 "/V" $2 "__" $3}' | xargs -I {} /exec-sql-script.sh {}
