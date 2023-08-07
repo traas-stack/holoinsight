178c9091c3aabcc5f211d48a7d97db66f1d971ab
霄鸿 <jinsong.yjs@antfin.com>
2023-08-07 20:26:48 +0800

    add log-json-generated.py

---
+++ test/scenes/common/log-json-generator.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+import atexit
+import datetime
+import sched
+import time
+
+import logwriter
+
+s = sched.scheduler(time.time, time.sleep)
+
+
+def schedule_with_fixed_rate(init_delay, interval, action, args):
+    def wrapper(*args2):
+        action()
+        now = datetime.datetime.now()
+        next_align_time = datetime.datetime.now().replace(microsecond=0) + datetime.timedelta(seconds=interval)
+        delta = next_align_time - now
+        s.enter(delta.total_seconds(), 1, wrapper, args2)
+
+    s.enter(init_delay, 1, wrapper, args)
+
+
+increase_value_log = logwriter.FileWrapper('test/json.log')
+atexit.register(increase_value_log.close)
+
+start = int(time.time())
+
+
+def task1(*args):
+    time_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    cur = int(time.time())
+    increase_value_log.write('{"time": "%s", "level":"%s", "biz":"biz1", "cost": "%dms"}\n' % (time_str, 'INFO', cur - start))
+    increase_value_log.flush()
+
+
+schedule_with_fixed_rate(0, 1, task1, ())
+
+s.run()
