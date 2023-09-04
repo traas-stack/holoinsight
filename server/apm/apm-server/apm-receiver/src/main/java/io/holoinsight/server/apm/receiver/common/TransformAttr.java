7eb981e4be83747b44b77670ed0df9c33a8e95bd
sanhuan.sw <sanhuan.sw@alibaba-inc.com>
2023-09-04 19:43:51 +0800

    feat: trace product close

--- server/apm/apm-server/apm-receiver/src/main/java/io/holoinsight/server/apm/receiver/common/TransformAttr.java
+++ server/apm/apm-server/apm-receiver/src/main/java/io/holoinsight/server/apm/receiver/common/TransformAttr.java
@@ -13,19 +13,41 @@ import java.util.Map;
 
 public class TransformAttr {
 
-  public static Map<String, AnyValue> attList2Map(List<KeyValue> attributes) {
-    Map<String, AnyValue> result = new HashMap<>(attributes.size());
+  public static Map<String, String> attList2Map(List<KeyValue> attributes) {
+    Map<String, String> result = new HashMap<>(attributes.size());
     for (io.opentelemetry.proto.common.v1.KeyValue attribute : attributes) {
-      result.put(attribute.getKey(), attribute.getValue());
+      result.put(attribute.getKey(), anyValueToString(attribute.getValue()));
 
       if (Const.REAL_TRACE_ID_TAGS.contains(attribute.getKey())) {
-        result.put(Const.REAL_TRACE_ID, attribute.getValue());
+        result.put(Const.REAL_TRACE_ID, anyValueToString(attribute.getValue()));
       } else if (Const.REAL_SPAN_ID_TAGS.contains(attribute.getKey())) {
-        result.put(Const.REAL_SPAN_ID, attribute.getValue());
+        result.put(Const.REAL_SPAN_ID, anyValueToString(attribute.getValue()));
       } else if (Const.REAL_PARENT_SPAN_ID_TAGS.contains(attribute.getKey())) {
-        result.put(Const.REAL_PARENT_SPAN_ID, attribute.getValue());
+        result.put(Const.REAL_PARENT_SPAN_ID, anyValueToString(attribute.getValue()));
       }
     }
     return result;
   }
+
+  public static String anyValueToString(AnyValue anyValue) {
+    switch (anyValue.getValueCase().getNumber()) {
+      case AnyValue.STRING_VALUE_FIELD_NUMBER:
+        return anyValue.getStringValue();
+      case AnyValue.BOOL_VALUE_FIELD_NUMBER:
+        return String.valueOf(anyValue.getBoolValue());
+      case AnyValue.INT_VALUE_FIELD_NUMBER:
+        return String.valueOf(anyValue.getIntValue());
+      case AnyValue.DOUBLE_VALUE_FIELD_NUMBER:
+        return String.valueOf(anyValue.getDoubleValue());
+      case AnyValue.ARRAY_VALUE_FIELD_NUMBER:
+        return String.valueOf(anyValue.getArrayValue());
+      case AnyValue.KVLIST_VALUE_FIELD_NUMBER:
+        return String.valueOf(anyValue.getKvlistValue());
+      case AnyValue.BYTES_VALUE_FIELD_NUMBER:
+        return String.valueOf(anyValue.getBytesValue());
+      default:
+        throw new UnsupportedOperationException("unsupported value type: " + anyValue);
+    }
+  }
+
 }
--- server/gateway/gateway-core/src/main/java/io/holoinsight/server/gateway/core/auth/ApikeyWebController.java
+++ server/gateway/gateway-core/src/main/java/io/holoinsight/server/gateway/core/auth/ApikeyWebController.java
@@ -3,14 +3,23 @@
  */
 package io.holoinsight.server.gateway.core.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.holoinsight.server.common.auth.ApikeyAuthService;
 import io.holoinsight.server.common.auth.AuthInfo;
+import io.holoinsight.server.common.ctl.MonitorProductCode;
+import io.holoinsight.server.common.ctl.ProductCtlService;
 import io.holoinsight.server.common.web.InternalWebApi;
-
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -21,8 +30,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @InternalWebApi
 public class ApikeyWebController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ApikeyWebController.class);
   @Autowired
   private ApikeyAuthService apikeyAuthService;
+  @Autowired
+  private ProductCtlService productCtlService;
 
   /**
    * <p>
@@ -31,10 +43,38 @@ public class ApikeyWebController {
    *
    * @param apikey
    */
-  @Deprecated
   @GetMapping({"/api/apikey/check", "/internal/api/gateway/apikey/check"})
-  public AuthInfo checkApikey(@RequestParam String apikey) {
-    return apikeyAuthService.getFromCache(apikey);
+  public Map<String, String> checkApikey(@RequestParam String apikey) {
+    Map<String, String> result = new HashMap<>();
+
+    apikey = getApikey(apikey, result);
+
+    AuthInfo authInfo = apikeyAuthService.getFromCache(apikey);
+    if (authInfo != null && StringUtils.isNotBlank(authInfo.getTenant())) {
+      result.put("tenant", authInfo.getTenant());
+    }
+
+    return result;
+  }
+
+  public String getApikey(String apikey, Map<String, String> result) {
+    try {
+      apikey = URLDecoder.decode(apikey);
+
+      if (apikey.startsWith("extend")) {
+        String[] extendInfoSplit = apikey.split("extend");
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> extendInfo = mapper.readValue(extendInfoSplit[1], Map.class);
+        apikey = extendInfo.get("authentication");
+
+        if (productCtlService.productClosed(MonitorProductCode.TRACE, extendInfo)) {
+          result.put("traceStatus", "false");
+        }
+      }
+    } catch (Exception e) {
+      LOGGER.error("[gateway]checkApikey error: ", e.getMessage());
+    }
+    return apikey;
   }
 
 }
