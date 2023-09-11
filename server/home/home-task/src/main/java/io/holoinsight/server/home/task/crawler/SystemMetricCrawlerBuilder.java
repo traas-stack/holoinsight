0a3c366cea48e40f81bff22714598a9e12db2444
霄鸿 <jinsong.yjs@antfin.com>
2023-09-11 13:44:43 +0800

    Merge remote-tracking branch 'origin' into jsy_0607

--- server/home/home-task/src/main/java/io/holoinsight/server/home/task/crawler/SystemMetricCrawlerBuilder.java
+++ server/home/home-task/src/main/java/io/holoinsight/server/home/task/crawler/SystemMetricCrawlerBuilder.java
@@ -66,45 +66,60 @@ public class SystemMetricCrawlerBuilder extends AbstractMetricCrawlerBuilder {
         .add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system", "TCP",
             "tcp_established", "k8s_pod_tcp_established", "tcp 连接被重置的每秒次数", BYTES_UNIT, 60, tags));
 
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "CPU", "cpu_util", "k8s_pod_cpu_util_5s", "cpu 使用率", PERCENT_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "CPU", "cpu_sys_5s", "k8s_pod_cpu_sys_5s", "cpu sys 使用率", PERCENT_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "CPU", "cpu_user_5s", "k8s_pod_cpu_user_5s", "cpu user 使用率", PERCENT_UNIT, 5, tags));
-    metricInfoList
-        .add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system", "CPU",
-            "cpu_inuse_cores_5s", "k8s_pod_cpu_inuse_cores_5s", "cpu 使用核数", NUMBER_UNIT, 5, tags));
-    metricInfoList
-        .add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system", "CPU",
-            "cpu_total_cores_5s", "k8s_pod_cpu_total_cores_5s", "cpu 总核数", NUMBER_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "MEMORY", "mem_uti_5s", "k8s_pod_mem_util_5s", "mem 使用率", PERCENT_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "MEMORY", "mem_used_5s", "k8s_pod_mem_used_5s", "mem 使用量", BYTES_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "MEMORY", "mem_total_5s", "k8s_pod_mem_total_5s", "mem 总量", BYTES_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "MEMORY", "mem_rss_5s", "k8s_pod_mem_rss_5s", "mem rss量", BYTES_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "MEMORY", "mem_cache_5s", "k8s_pod_mem_cache_5s", "mem cache量", BYTES_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "LOAD", "load_load1_5s", "k8s_pod_load_load1_5s", "load1量", NUMBER_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "LOAD", "load_load5_5s", "k8s_pod_load_load5_5s", "load5量", NUMBER_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "LOAD", "load_load15_5s", "k8s_pod_load_load15_5s", "load15量", NUMBER_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "OOM", "oom", "k8s_pod_oom_5s", "oom次数", NUMBER_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "TRAFFIC", "traffic_bytin_5s", "k8s_pod_traffic_bytin_5s", "物理主机物理网卡rx方向的流量大小量", BYTES_UNIT,
-        5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "TRAFFIC", "traffic_bytout_5s", "k8s_pod_traffic_bytout_5s", "物理主机物理网卡tx方向的流量大小",
-        BYTES_UNIT, 5, tags));
-    metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system",
-        "TCP", "tcp_established_5s", "k8s_pod_tcp_established_5s", "tcp 连接被重置的每秒次数", BYTES_UNIT, 5,
-        tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "CPU", "cpu_util", "k8s_pod_cpu_util_5s", "cpu 使用率", PERCENT_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "CPU", "cpu_sys_5s", "k8s_pod_cpu_sys_5s", "cpu sys 使用率", PERCENT_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "CPU", "cpu_user_5s", "k8s_pod_cpu_user_5s", "cpu user 使用率", PERCENT_UNIT, 5, tags));
+    // metricInfoList
+    // .add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system", "CPU",
+    // "cpu_inuse_cores_5s", "k8s_pod_cpu_inuse_cores_5s", "cpu 使用核数", NUMBER_UNIT, 5, tags));
+    // metricInfoList
+    // .add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION, "system", "CPU",
+    // "cpu_total_cores_5s", "k8s_pod_cpu_total_cores_5s", "cpu 总核数", NUMBER_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "MEMORY", "mem_uti_5s", "k8s_pod_mem_util_5s", "mem 使用率", PERCENT_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "MEMORY", "mem_used_5s", "k8s_pod_mem_used_5s", "mem 使用量", BYTES_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "MEMORY", "mem_total_5s", "k8s_pod_mem_total_5s", "mem 总量", BYTES_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "MEMORY", "mem_rss_5s", "k8s_pod_mem_rss_5s", "mem rss量", BYTES_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "MEMORY", "mem_cache_5s", "k8s_pod_mem_cache_5s", "mem cache量", BYTES_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "LOAD", "load_load1_5s", "k8s_pod_load_load1_5s", "load1量", NUMBER_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "LOAD", "load_load5_5s", "k8s_pod_load_load5_5s", "load5量", NUMBER_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "LOAD", "load_load15_5s", "k8s_pod_load_load15_5s", "load15量", NUMBER_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "OOM", "oom", "k8s_pod_oom_5s", "oom次数", NUMBER_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "TRAFFIC", "traffic_bytin_5s", "k8s_pod_traffic_bytin_5s", "物理主机物理网卡rx方向的流量大小量", BYTES_UNIT,
+    // 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "TRAFFIC", "traffic_bytout_5s", "k8s_pod_traffic_bytout_5s", "物理主机物理网卡tx方向的流量大小",
+    // BYTES_UNIT, 5, tags));
+    // metricInfoList.add(genMetricInfo(GLOBAL_TENANT, GLOBAL_WORKSPACE, GLOBAL_ORGANIZATION,
+    // "system",
+    // "TCP", "tcp_established_5s", "k8s_pod_tcp_established_5s", "tcp 连接被重置的每秒次数", BYTES_UNIT, 5,
+    // tags));
 
     return metricInfoList;
   }
