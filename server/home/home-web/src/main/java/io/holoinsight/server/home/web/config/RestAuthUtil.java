5e949549147bc080178c8a441457f14b7b4532e7
霄鸿 <jinsong.yjs@antfin.com>
2023-11-02 11:58:55 +0800

    refactor(home): optimize restauthutil

--- server/home/home-web/src/main/java/io/holoinsight/server/home/web/config/RestAuthUtil.java
+++ server/home/home-web/src/main/java/io/holoinsight/server/home/web/config/RestAuthUtil.java
@@ -18,9 +18,8 @@ public class RestAuthUtil {
 
   public static RestAuthUtil singleton = new RestAuthUtil();
 
-  public static final Set<String> NO_AUTH_PATH =
-      Collections.unmodifiableSet(new HashSet<>(Arrays.asList("/webapi/sys/config",
-          "/webapi/token/apply", "/webapi/sys/time", "/openapi/microapp/monitor/v2/query",
+  public static final Set<String> NO_AUTH_PATH = Collections.unmodifiableSet(
+      new HashSet<>(Arrays.asList("/webapi/sys/config", "/webapi/token/apply", "/webapi/sys/time",
           "/webapi/user/logout", "/webapi/sys/checkservice", "/api/v1/prometheus/write")));
 
   public static final Set<String> AUTH_PATH =
