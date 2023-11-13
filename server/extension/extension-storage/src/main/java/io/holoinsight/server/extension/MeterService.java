9793d524b8eac6be566792d20a4b3fd1ed0ca0af
saimu.msm <saimu.msm@antfin.com>
2023-11-13 15:10:47 +0800

    meter for detail

--- server/extension/extension-storage/src/main/java/io/holoinsight/server/extension/MeterService.java
+++ server/extension/extension-storage/src/main/java/io/holoinsight/server/extension/MeterService.java
@@ -5,6 +5,8 @@ package io.holoinsight.server.extension;
 
 import io.holoinsight.server.extension.model.WriteMetricsParam;
 
+import java.util.Map;
+
 /**
  * @author zanghaibo
  * @time 2023-05-23 10:39 上午
@@ -16,4 +18,11 @@ public interface MeterService {
    * @param writeMetricsParam
    */
   void meter(WriteMetricsParam writeMetricsParam);
+
+  /**
+   * meter one single point
+   * 
+   * @param tags
+   */
+  void meter(Map<String, String> tags, String tenant, String metricName);
 }
