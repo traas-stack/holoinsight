d61eb11ffc6f553f66b06675b7bd4b8a00590b6d
saimu.msm <saimu.msm@antfin.com>
2023-12-07 15:38:15 +0800

    fix license

--- server/home/home-web/src/main/java/io/holoinsight/server/home/web/common/SecurityResource.java
+++ server/home/home-web/src/main/java/io/holoinsight/server/home/web/common/SecurityResource.java
@@ -1,5 +1,5 @@
 /*
- * Alipay.com Inc. Copyright (c) 2004-2018 All Rights Reserved.
+ * Copyright 2022 Holoinsight Project Authors. Licensed under Apache-2.0.
  */
 package io.holoinsight.server.home.web.common;
 
