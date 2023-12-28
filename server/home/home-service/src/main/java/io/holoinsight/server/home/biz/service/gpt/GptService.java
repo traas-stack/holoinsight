330d5720d1d4b27ade1ace9c730e967a52d76906
wsy327643 <wsy327643@antgroup.com>
2023-12-28 10:23:56 +0800

    format file

--- server/home/home-service/src/main/java/io/holoinsight/server/home/biz/service/gpt/GptService.java
+++ server/home/home-service/src/main/java/io/holoinsight/server/home/biz/service/gpt/GptService.java
@@ -26,72 +26,70 @@ import java.util.UUID;
 @Service
 public class GptService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GptService.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(GptService.class);
 
-    @Autowired
-    private EnvironmentProperties environmentProperties;
+  @Autowired
+  private EnvironmentProperties environmentProperties;
 
 
-    public String submit(Map<String, Object> request, String traceId){
-        String submitUrl = environmentProperties.getGptUrl() + "/ai/task/submit";
-        if (request != null && !request.isEmpty()) {
-            LOGGER.info("{} [submit],url={},requestBody={}", traceId, submitUrl, JSON.toJSONString(request));
-            return post(submitUrl, JSON.toJSONString(request), traceId);
-        }
-        return "FALSE";
+  public String submit(Map<String, Object> request, String traceId) {
+    String submitUrl = environmentProperties.getGptUrl() + "/ai/task/submit";
+    if (request != null && !request.isEmpty()) {
+      LOGGER.info("{} [submit],url={},requestBody={}", traceId, submitUrl,
+          JSON.toJSONString(request));
+      return post(submitUrl, JSON.toJSONString(request), traceId);
+    }
+    return "FALSE";
+  }
+
+  public String save(List<Map<String, Object>> request, String traceId) {
+    String saveUrl = environmentProperties.getGptUrl() + "/qa/save";
+    if (CollectionUtils.isNotEmpty(request)) {
+      LOGGER.info("{} [submit],url={},requestBody={}", traceId, saveUrl,
+          JSON.toJSONString(request));
+      request.forEach(req -> post(saveUrl, JSON.toJSONString(req), traceId));
+      return "SUCCESS";
+    } else {
+      return "FALSE";
     }
 
-    public String save(List<Map<String, Object>> request, String traceId){
-        String saveUrl = environmentProperties.getGptUrl() + "/qa/save";
-        if (CollectionUtils.isNotEmpty(request)) {
-            LOGGER.info("{} [submit],url={},requestBody={}", traceId, saveUrl, JSON.toJSONString(request));
-            request.forEach(req -> post(saveUrl, JSON.toJSONString(req), traceId));
-            return "SUCCESS";
+  }
+
+  public String post(String url, String requestBody, String traceId) {
+
+    XHttpResponse response = null;
+    String resultString = null;
+    String detailTraceId = UUID.randomUUID().toString();
+    long start = System.currentTimeMillis();
+    try {
+      XHttpRequest req = XHttpRequest.post(url, Collections.emptyMap(), "utf-8", 20000,
+          requestBody.getBytes(), "application/json; charset=utf-8");
+      response = HttpProxy.request(req);
+
+      if (response != null) {
+        resultString = response.getStringResponse();
+        if (response.code != 200) {
+          LOGGER.error("{} {} [InvokeError],url={},requestBody={},response={},content={}", traceId,
+              detailTraceId, url, requestBody, response.code, resultString);
+          return null;
         } else {
-            return "FALSE";
+          if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("{} [InvokeDetail],url={}, requestBody={},response={},content={}",
+                detailTraceId, url, requestBody, response.code, resultString);
+          }
         }
+      } else {
+        LOGGER.error("{} {} [InvokeError],url={}, response is null", traceId, detailTraceId, url);
+      }
 
+    } catch (Exception e) {
+      LOGGER.error("{} {} [InvokeException] Sync Exception for {}", traceId, detailTraceId, url, e);
+    } finally {
+      long cost = System.currentTimeMillis() - start;
+      int code = response == null ? 500 : response.code;
+      LOGGER.info("{} {} [InvokeInfo],url={},responseCode={},cost={}", traceId, detailTraceId, url,
+          code, cost);
     }
-
-    public String post(String url, String requestBody, String traceId) {
-
-        XHttpResponse response = null;
-        String resultString = null;
-        String detailTraceId = UUID.randomUUID().toString();
-        long start = System.currentTimeMillis();
-        try {
-            XHttpRequest req = XHttpRequest.post(url, Collections.emptyMap(), "utf-8", 20000,
-                    requestBody.getBytes(), "application/json; charset=utf-8");
-            response = HttpProxy.request(req);
-
-            if (response != null) {
-                resultString = response.getStringResponse();
-                if (response.code != 200) {
-                    LOGGER.error(
-                            "{} {} [InvokeError],url={},requestBody={},response={},content={}",
-                            traceId, detailTraceId, url, requestBody, response.code, resultString);
-                    return null;
-                } else {
-                    if (LOGGER.isDebugEnabled()) {
-                        LOGGER.debug(
-                                "{} [InvokeDetail],url={}, requestBody={},response={},content={}",
-                                detailTraceId, url, requestBody, response.code, resultString);
-                    }
-                }
-            } else {
-                LOGGER.error("{} {} [InvokeError],url={}, response is null", traceId,
-                        detailTraceId, url);
-            }
-
-        } catch (Exception e) {
-            LOGGER.error("{} {} [InvokeException] Sync Exception for {}", traceId, detailTraceId,
-                    url, e);
-        } finally {
-            long cost = System.currentTimeMillis() - start;
-            int code = response == null ? 500 : response.code;
-            LOGGER.info("{} {} [InvokeInfo],url={},responseCode={},cost={}", traceId,
-                    detailTraceId, url, code, cost);
-        }
-        return resultString;
-    }
+    return resultString;
+  }
 }
