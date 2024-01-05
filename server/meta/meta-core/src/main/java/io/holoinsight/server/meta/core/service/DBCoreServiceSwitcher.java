b688470c7d58ec3fbb81c68f964c1705e2d63f07
jsy <jsy1001de@163.com>
2024-01-05 13:18:57 +0800

    fix: fix dbCoreService switch (#775)

--- server/meta/meta-core/src/main/java/io/holoinsight/server/meta/core/service/DBCoreServiceSwitcher.java
+++ server/meta/meta-core/src/main/java/io/holoinsight/server/meta/core/service/DBCoreServiceSwitcher.java
@@ -3,15 +3,18 @@
  */
 package io.holoinsight.server.meta.core.service;
 
+import io.holoinsight.server.common.dao.entity.MetaDataDictValue;
 import io.holoinsight.server.common.service.SuperCacheService;
 import io.holoinsight.server.meta.core.service.bitmap.BitmapDataCoreService;
 import io.holoinsight.server.meta.core.service.hashmap.HashMapDataCoreService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
 @Service
+@Slf4j
 public class DBCoreServiceSwitcher {
 
   @Autowired
@@ -24,13 +27,15 @@ public class DBCoreServiceSwitcher {
   private SuperCacheService superCacheService;
 
   public DBCoreService dbCoreService() {
-    boolean metaIndexBitMapEnable = true;
+    boolean metaIndexBitMapEnable = false;
     try {
-      metaIndexBitMapEnable = Boolean.parseBoolean(superCacheService.getSc().metaDataDictValueMap
-          .getOrDefault("global_config", new HashMap<>()).get("meta_index_bitmap_enable")
-          .getDictValue());
+      MetaDataDictValue metaDataDictValue = superCacheService.getSc().metaDataDictValueMap
+          .getOrDefault("global_config", new HashMap<>()).get("meta_index_bitmap_enable");
+      if (null != metaDataDictValue) {
+        metaIndexBitMapEnable = Boolean.parseBoolean(metaDataDictValue.getDictValue());
+      }
     } catch (Exception e) {
-      metaIndexBitMapEnable = true;
+      log.warn("meta_index_bitmap_enable value illegal");
     }
     DBCoreService service = metaIndexBitMapEnable ? bitmapDataCoreService : hashMapDataCoreService;
     service.startBuildIndex();
