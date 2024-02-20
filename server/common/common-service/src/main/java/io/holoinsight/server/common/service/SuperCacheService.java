6cf423172cd2fb5baf6ff78b554da5ac5b888389
霄鸿 <jinsong.yjs@antfin.com>
2024-02-20 20:16:51 +0800

    fix: fix monitorinstance in supercacheservice

--- server/common/common-service/src/main/java/io/holoinsight/server/common/service/SuperCacheService.java
+++ server/common/common-service/src/main/java/io/holoinsight/server/common/service/SuperCacheService.java
@@ -20,8 +20,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  *
@@ -53,13 +51,16 @@ public class SuperCacheService extends ScheduleLoadTask {
     ProdLog.info("[SuperCache] load start");
     SuperCache sc = new SuperCache();
     sc.metaDataDictValueMap = metaDictValueService.getMetaDictValue();
+    ProdLog.info("[SuperCache][metaDataDictValueMap] size: " + sc.metaDataDictValueMap.size());
     sc.expressionMetricList = metricInfoService.querySpmList();
+    ProdLog.info("[SuperCache][expressionMetricList] size: " + sc.expressionMetricList.size());
     sc.workspaceTenantMap = new HashMap<>();
     QueryWrapper<MonitorInstance> queryWrapper = new QueryWrapper<>();
-    queryWrapper.eq("deleted", false);
+    queryWrapper.eq("deleted", 0);
     List<MonitorInstance> instances = monitorInstanceService.list(queryWrapper);
     instances.forEach(
-        instance -> sc.workspaceTenantMap.put(instance.getWorkspace(), instance.getTenant()));
+            instance -> sc.workspaceTenantMap.put(instance.getWorkspace(), instance.getTenant()));
+    ProdLog.info("[SuperCache][workspaceTenantMap] size: " + sc.workspaceTenantMap.size());
     queryMetricInfoByPage(sc);
     this.sc = sc;
     ProdLog.info("[SuperCache] load end");
@@ -97,14 +98,16 @@ public class SuperCacheService extends ScheduleLoadTask {
         }
         map.put(metricInfo.getMetricTable(), metricInfo);
         if (StringUtils.isNotBlank(metricInfo.getWorkspace())
-            && !StringUtils.equals(metricInfo.getWorkspace(), "-")) {
+                && !StringUtils.equals(metricInfo.getWorkspace(), "-")) {
           List<MetricInfo> list =
-              workspaceMap.computeIfAbsent(metricInfo.getWorkspace(), k -> new ArrayList<>());
+                  workspaceMap.computeIfAbsent(metricInfo.getWorkspace(), k -> new ArrayList<>());
           list.add(metricInfo);
         }
       }
     } while (current < 1000);
     sc.metricInfoMap = map;
     sc.workspaceMetricInfoMap = workspaceMap;
+    ProdLog.info("[SuperCache][metricInfoMap] size: " + sc.metricInfoMap.size());
+    ProdLog.info("[SuperCache][workspaceMetricInfoMap] size: " + sc.workspaceMetricInfoMap.size());
   }
 }
