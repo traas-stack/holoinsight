ae27a6cfe9619c6d4e8b5b678ed02feed1da44da
masaimu <masaimu@126.com>
2024-03-28 17:51:27 +0800

    m

--- server/home/home-web/src/main/java/io/holoinsight/server/home/web/security/custom/AlarmRuleLevelAuthorizationChecker.java
+++ server/home/home-web/src/main/java/io/holoinsight/server/home/web/security/custom/AlarmRuleLevelAuthorizationChecker.java
@@ -513,9 +513,9 @@ public class AlarmRuleLevelAuthorizationChecker extends AbstractQueryChecker
     if (StringUtils.isNotEmpty(extra.sourceLink)) {
       return failCheckResult("sourceLink %s should be empty", extra.sourceLink);
     }
-    if (StringUtils.isNotEmpty(extra.md5)) {
-      return failCheckResult("md5 %s should be empty", extra.md5);
-    }
+    // if (StringUtils.isNotEmpty(extra.md5)) {
+    // return failCheckResult("md5 %s should be empty", extra.md5);
+    // }
     if (!CollectionUtils.isEmpty(extra.alertTags) && !checkSqlField(extra.alertTags)) {
       return failCheckResult("fail to check sql field in alertTags %s", J.toJson(extra.alertTags));
     }
